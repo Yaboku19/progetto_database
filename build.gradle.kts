afd99db151a0b92e2cb3a40b34ba3a12e1fa48aa
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-06-20 11:03:03 +0200

    test with javafx

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,20 @@ val javaFXModules = listOf(
     "media"
 )
 
+val supportedPlatforms = listOf("linux", "mac", "win")
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     implementation("mysql:mysql-connector-java:8.0.29")
+
+    val javaFxVersion = 19
+
+    for (platform in supportedPlatforms) {
+        for (module in javaFXModules) {
+            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
+        }
+    }
 }
 
 application {
--- src/main/java/futureodissey/view/impl/javafx/ViewImpl.java
+++ src/main/java/futureodissey/view/impl/javafx/ViewImpl.java
@@ -2,6 +2,21 @@ package futureodissey.view.impl.javafx;
 
 import futureodissey.view.api.View;
 
-public class ViewImpl implements View{
-    
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class ViewImpl implements View {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
