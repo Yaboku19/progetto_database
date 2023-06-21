3bd0cbf8dc0630d9ab7668165d593dc66b18899b
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-06-21 14:49:01 +0200

    creation of DisponibilitaTable

--- src/main/java/futureodissey/db/impl/RichestaTable.java
+++ src/main/java/futureodissey/db/impl/RichestaTable.java
@@ -8,34 +8,34 @@ import java.util.List;
 import java.util.Optional;
 import futureodissey.db.api.AbstractTable;
 import futureodissey.db.api.Table;
-import futureodissey.model.impl.rowtype.Disponibilita;
+import futureodissey.model.impl.rowtype.Richiesta;
 import javafx.util.Pair;
 
-public class DisponibilitaTable extends AbstractTable<Disponibilita> implements Table<Disponibilita, Pair<String, String>>{
+public class RichestaTable extends AbstractTable<Richiesta> implements Table<Richiesta, Pair<String, Integer>> {
     private final String key1 = "nomeRisorsa";
-    private final String key2 = "nomeFazione";
+    private final String key2 = "codiceTaskType";
     private final String quantita = "quantita";
 
-    public DisponibilitaTable(Connection connection) {
-        super("disponibilita", connection);
+    public RichestaTable(Connection connection) {
+        super("richiesta", connection);
     }
 
     @Override
     public boolean createTable() {
         return createTablePrivate("CREATE TABLE " + tableName + " (" +
             key1 + " CHAR(40) NOT NULL," +
-            key2 + " CHAR(40) NOT NULL," +
+            key2 + " INT NOT NULL," +
             quantita + " INT NOT NULL," +
             "PRIMARY KEY (" + key1 + ", " + key2 +
             "))");
     }
 
     @Override
-    public Optional<Disponibilita> findByPrimaryKey(Pair<String, String> primaryKey) {
+    public Optional<Richiesta> findByPrimaryKey(Pair<String, Integer> primaryKey) {
         return findByPrimaryKeyPrivate(" WHERE " + key1 + " = ? AND " + key2 + " = ?", st -> {
             try {
                 st.setString(1, primaryKey.getKey());
-                st.setString(2, primaryKey.getValue());
+                st.setInt(2, primaryKey.getValue());
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -43,12 +43,12 @@ public class DisponibilitaTable extends AbstractTable<Disponibilita> implements 
     }
 
     @Override
-    public boolean save(Disponibilita disponibilita) {
-        return savePrivate(disponibilita, " VALUES(?, ?, ?)", st -> {
+    public boolean save(Richiesta richiesta) {
+        return savePrivate(richiesta, " VALUES(?, ?, ?)", st -> {
             try {
-                st.setString(1, disponibilita.getNomeRisorsa());
-                st.setString(2, disponibilita.getNomeFazione());
-                st.setInt(3, disponibilita.getQuantita());
+                st.setString(1, richiesta.getNomeRisorsa());
+                st.setInt(2, richiesta.getCodiceTaskType());
+                st.setInt(3, richiesta.getQuantita());
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -56,11 +56,11 @@ public class DisponibilitaTable extends AbstractTable<Disponibilita> implements 
     }
 
     @Override
-    public boolean delete(Pair<String, String> primaryKey) {
+    public boolean delete(Pair<String, Integer> primaryKey) {
         return deletePrivate(" WHERE " + key1 + " = ? AND " + key2 + " = ?", st -> {
             try {
                 st.setString(1, primaryKey.getKey());
-                st.setString(2, primaryKey.getValue());
+                st.setInt(2, primaryKey.getValue());
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -68,12 +68,12 @@ public class DisponibilitaTable extends AbstractTable<Disponibilita> implements 
     }
 
     @Override
-    public boolean update(Disponibilita disponibilita) {
-        return updatePrivate(disponibilita, " SET " + quantita + " = ? WHERE " + key1 + " = ? AND " + key2 + " = ?", st -> {
+    public boolean update(Richiesta richiesta) {
+        return updatePrivate(richiesta, " SET " + quantita + " = ? WHERE " + key1 + " = ? AND " + key2 + " = ?", st -> {
             try {
-                st.setInt(1, disponibilita.getQuantita());
-                st.setString(2, disponibilita.getNomeRisorsa());
-                st.setString(3, disponibilita.getNomeFazione());
+                st.setInt(1, richiesta.getQuantita());
+                st.setString(2, richiesta.getNomeRisorsa());
+                st.setInt(3, richiesta.getCodiceTaskType());
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -81,13 +81,13 @@ public class DisponibilitaTable extends AbstractTable<Disponibilita> implements 
     }
 
     @Override
-    protected List<Disponibilita> readStudentsFromResultSet(ResultSet resultSet) {
-        List<Disponibilita> result = new ArrayList<>();
+    protected List<Richiesta> readStudentsFromResultSet(ResultSet resultSet) {
+        List<Richiesta> result = new ArrayList<>();
         try {
             while(resultSet.next()) {
-                result.add(new Disponibilita(
+                result.add(new Richiesta(
                     resultSet.getString(key1), 
-                    resultSet.getString(key2),
+                    resultSet.getInt(key2),
                     resultSet.getInt(quantita)));
             }
         } catch (SQLException e) {
