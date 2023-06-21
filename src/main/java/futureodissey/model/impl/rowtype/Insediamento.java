f147efcc5aa368b18c053c48f023417f5bb514a3
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-06-21 11:27:49 +0200

    creation of Insediamento class

---
+++ src/main/java/futureodissey/model/impl/rowtype/Insediamento.java
@@ -0,0 +1,47 @@
+package futureodissey.model.impl.rowtype;
+
+import java.util.Objects;
+
+public class Insediamento {
+    private final String nomeFazione;
+    private final String nomeInsediamento;
+    private final String nomePianeta;
+    
+    public Insediamento(final String nomeFazione, final String nomeInsediamento, final String NomePianeta) {
+        this.nomeFazione = Objects.requireNonNull(nomeFazione);
+        this.nomeInsediamento = Objects.requireNonNull(nomeInsediamento);
+        this.nomePianeta = Objects.requireNonNull(NomePianeta);
+    }
+    
+    public String getNomeFazione() {
+        return this.nomeFazione;
+    }
+
+    public String getNomeInsediamento() {
+        return nomeInsediamento;
+    }
+
+    public String getNomePianeta() {
+        return nomePianeta;
+    }
+    
+    @Override
+    public String toString() {
+        return new StringBuilder()
+            .append("(").append(nomeFazione).append(") ")
+            .append(nomeInsediamento).append(" - ").append(nomePianeta).toString();
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return (other instanceof Insediamento)
+            && ((Insediamento) other).getNomeFazione().equals(this.getNomeFazione())
+            && ((Insediamento) other).getNomeInsediamento().equals(this.getNomeInsediamento())
+            && ((Insediamento) other).getNomePianeta().equals(this.getNomePianeta());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomeFazione, nomeInsediamento, nomePianeta);
+    }
+}
