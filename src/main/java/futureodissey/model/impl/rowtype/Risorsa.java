f69ed0f564884ae64da70d25db0c5193b454c551
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-06-21 12:43:54 +0200

    creation of Risorsa class

---
+++ src/main/java/futureodissey/model/impl/rowtype/Risorsa.java
@@ -0,0 +1,32 @@
+package futureodissey.model.impl.rowtype;
+
+import java.util.Objects;
+
+public class Risorsa {
+    private final String nomeRisorsa;
+
+    public Risorsa(final String nomeRisorsa) {
+        this.nomeRisorsa = nomeRisorsa;
+    }
+
+     public String getNomeRisorsa() {
+        return this.nomeRisorsa;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+            .append("(").append(nomeRisorsa).append(") ").toString();
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return (other instanceof Risorsa)
+            && ((Risorsa) other).getNomeRisorsa().equals(this.getNomeRisorsa());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomeRisorsa);
+    }
+}
