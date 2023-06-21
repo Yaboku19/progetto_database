c12b2f6c79617d7d0ab11215bd4828c3947af663
Yaboku19 <emanuele.martelli5@studio.unibo.it>
2023-06-21 12:13:49 +0200

    creation of Pianeta class

---
+++ src/main/java/futureodissey/model/impl/rowtype/Pianeta.java
@@ -0,0 +1,40 @@
+package futureodissey.model.impl.rowtype;
+
+import java.util.Objects;
+
+public class Pianeta {
+    private final String nomePianeta;
+    private final String nomeRisorsa;
+
+    public Pianeta(final String nomePianeta, final String nomeRisorsa) {
+        this.nomePianeta = nomePianeta;
+        this.nomeRisorsa = nomeRisorsa;
+    }
+
+    public String getNomePianeta() {
+        return this.nomePianeta;
+    }
+
+    public String getNomeRisorsa() {
+        return this.nomeRisorsa;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+            .append("(").append(nomePianeta).append(") ")
+            .append(nomeRisorsa).toString();
+    }
+
+    @Override
+    public boolean equals(final Object other) {
+        return (other instanceof Pianeta)
+            && ((Pianeta) other).getNomePianeta().equals(this.getNomePianeta())
+            && ((Pianeta) other).getNomeRisorsa().equals(this.getNomeRisorsa());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nomePianeta, nomeRisorsa);
+    }
+}
